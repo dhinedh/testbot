c0def68b1a2ed7d44cbc656fb2c3365c9980944d
zechsoft <zechsoft.it@gmail.com>
2026-05-26 10:27:59 +0530

    Fix: Remove custom outDir so Vite defaults to dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../public',
-    emptyOutDir: true, // also necessary
-  },
   server: {
     proxy: {
       '/crm': 'http://localhost:3000',
